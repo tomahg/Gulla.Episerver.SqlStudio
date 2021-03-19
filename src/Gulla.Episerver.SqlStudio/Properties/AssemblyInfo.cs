61a63aee7438fef16801b9ba2893475394851d54
Tomas Hensrud Gulla <tomas.h.gulla@novacare.no>
2021-03-19 15:21:41 +0100

    Update version + plugin metadarta

--- src/Gulla.Episerver.SqlStudio/Properties/AssemblyInfo.cs
+++ src/Gulla.Episerver.SqlStudio/Properties/AssemblyInfo.cs
@@ -1,17 +1,19 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using EPiServer.PlugIn;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Gulla.Episerver.SqlStudio")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Adds an admin tool that let you query the SQL database from within Episerver UI.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Tomas Hensrud Gulla")]
 [assembly: AssemblyProduct("Gulla.Episerver.SqlStudio")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: PlugInSummary("https://github.com/tomahg/gulla.episerver.sqlstudio", LicensingMode.CustomLicense)]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -31,5 +33,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
