0c95c8a68c0084f6c8f1d72cc2ac1c227f4459ac
Tomas Hensrud Gulla <tomas.h.gulla@novacare.no>
2024-10-27 08:18:29 +0100

    Update to .NET 8

--- src/build-package.ps1
+++ src/build-package.ps1
@@ -1,4 +1,4 @@
 dotnet build .\Gulla.Episerver.SqlStudio\Gulla.Episerver.SqlStudio.csproj -c Release
 dotnet pack .\Gulla.Episerver.SqlStudio\Gulla.Episerver.SqlStudio.csproj -c Release
 
-move .\Gulla.Episerver.SqlStudio\bin\Release\*.nupkg ..\..\..\Nuget
+move .\Gulla.Episerver.SqlStudio\bin\Release\*.nupkg ..\..\Nuget
